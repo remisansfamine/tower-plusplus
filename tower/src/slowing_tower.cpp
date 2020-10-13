8ba7b83b6209a67161d2fe2e6918b9b4ee392f11
GINER Remi <r.giner@student.isartdigital.com>
2020-10-13 17:36:10 +0200

    balancing

--- tower/src/slowing_tower.cpp
+++ tower/src/slowing_tower.cpp
@@ -5,8 +5,8 @@ SlowingTower::SlowingTower(Vector2 position, const ResourceManager& RM)
 {
     m_life = Life(40);
     m_damage = 2.5f;
-    m_cooldown = m_fire_rate = 0.75f;
-    m_range = 500;
+    m_cooldown = m_fire_rate = 0.25f;
+    m_range = 175;
     m_texture = RM.get_texture(TextureType::SLOWING_IDLE);
 }
 
